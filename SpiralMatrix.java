7ad0a2a764c53839db8bcf688ce20b322c6e2c70
Bismoy Murasing <bismoy@grabhouse.com>
2015-09-12 03:05:06 +0530

    Very easy way of solving

---
+++ SpiralMatrix.java
@@ -0,0 +1,42 @@
+
+public class SpiralMatrix {
+	
+	private static void Sizer (int[][] mat) {
+		int row = 0;
+		
+		while(row < mat.length) {
+			
+			
+			if(row % 2 == 0) {
+				int col = 0;
+			while(col < mat[0].length) {
+				System.out.print(mat[row][col]+" ");
+				col++;
+			} 
+			}
+			
+			if(row % 2 == 1) {
+				int col = mat[0].length-1;
+			while(col >= 0) {
+				System.out.print(mat[row][col]+" ");
+				col--;
+			} 
+			}
+			
+			row++;
+			
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+		int[][] mat = {  {1,2,3,10},
+				         {4,5,6,11},
+				         {7,8,9,12} ,
+				         {30,40,50,60}
+				      };
+		Sizer(mat);
+		
+	}
+
+}
